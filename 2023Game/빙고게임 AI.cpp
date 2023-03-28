0b19c545f56b55266dcdd9bddb8b2e84b6c73203
Chae min seo <98958229+ms9876@users.noreply.github.com>
2023-03-29 00:23:10 +0900

    빙고게임 노말모드 도전 (하지만 실패ㅜㅜ)

--- 2023Game/빙고게임 AI.cpp
+++ 2023Game/빙고게임 AI.cpp
@@ -1,32 +1,34 @@
-#include <iostream>
-#include <Windows.h> // sleep
+#include<iostream>
+#include<Windows.h> // sleep
 using namespace std;
-enum class AI_MODE {
-	AM_EASY,
+
+enum class AI_MODE
+{
+	AM_EASY = 1,
 	AM_NORMAL
 };
+
 void Init(int* _pNumber)
 {
 	// 셔플
-	//srand((unsigned int)time(NULL));
+	// 값 다 들어가있어.
 	for (int i = 0; i < 25; i++)
 		_pNumber[i] = i + 1;
-
+	// 섞어요.
 	int iTemp, idx1, idx2;
 	for (int i = 0; i < 100; i++)
 	{
-		idx1 = rand() % 25;
+		idx1 = rand() % 25; // 0 ~ 23
 		idx2 = rand() % 25;
 		iTemp = _pNumber[idx1];
 		_pNumber[idx1] = _pNumber[idx2];
 		_pNumber[idx2] = iTemp;
 	}
 }
+// 값, 참조, 주소.
 void RenderNumber(int* _pNumber, int& _iBingo)
 {
-
-
-	// 숫자를 출력
+	// 숫자를 출력해줄거야.
 	for (int i = 0; i < 5; i++)
 	{
 		for (int j = 0; j < 5; j++)
@@ -38,93 +40,148 @@ void RenderNumber(int* _pNumber, int& _iBingo)
 		}
 		cout << endl;
 	}
-	cout << "bingo Line : " << _iBingo << endl;
+	cout << "Bingo Line: " << _iBingo << endl;
 }
-void ChangeNumber(int* _pNumber, int& _iBingo) {
-	for (int i = 0; i < 25; i++) {
-		if (_pNumber) {
+void ChangeNumber(int* _pNumber, int& _iInput)
+{
+	// 입력된 숫자와 현재 빙고판에서 일치하는지 확인을 하고,
+	// INT_MAX
+	for (int i = 0; i < 25; i++)
+	{
+		if (_pNumber[i] == _iInput)
 			_pNumber[i] = INT_MAX;
-		}
 	}
 }
-int BingoCount(int* _pNumber) {
+int BingoCount(int* _pNumber)
+{
 	int iCntBingo = 0;
 	int iHorStar = 0, iVerStar = 0, iLTStar = 0, iRTStar = 0;
-
-	for (int i = 0; i < 5; i++) {
+	// 가로, 세로, 대각선을 체크해줄겁니다.
+	for (int i = 0; i < 5; i++)
+	{
 		iHorStar = iVerStar = 0;
-		for (int j = 0; j < 5; j++) {
-			if (_pNumber[i * 5 * j] == INT_MAX)
+		for (int j = 0; j < 5; j++)
+		{
+			if (_pNumber[i * 5 + j] == INT_MAX)
 				iHorStar++;
-			if (_pNumber[j * 5 * i] == INT_MAX)
+			if (_pNumber[j * 5 + i] == INT_MAX)
 				iVerStar++;
-
 		}
 		if (iHorStar == 5)
 			iCntBingo++;
+		if (iVerStar == 5)
+			iCntBingo++;
 	}
-	for (int i = 0; i < 25; i += 6) {
-		if (_pNumber[i] == INT_MAX) {
+
+	// 대각선
+	for (int i = 0; i < 25; i += 6)
+	{
+		if (_pNumber[i] == INT_MAX)
 			iLTStar++;
-		}
 	}
 	if (iLTStar == 5)
 		iCntBingo++;
-	for (int i = 4; i < 20; i += 4) {
-		if (_pNumber[i] == INT_MAX) {
+	for (int i = 4; i <= 20; i += 4)
+	{
+		if (_pNumber[i] == INT_MAX)
 			iRTStar++;
-		}
 	}
 	if (iRTStar == 5)
 		iCntBingo++;
 	return iCntBingo;
 }
-AI_MODE SelcetAiMode() {
-	cout << "====================" << endl;
-	cout << "|     빙고 게임     |" << endl;
-	cout << "====================" << endl;
-
-	cout << "==============================================================" << endl;
-	cout << "            빙고줄이 5줄 이상이면 게임에서 승리합니다!           " << endl;
-	cout << "==============================================================" << endl;
-	cout << "1. EASY" << endl;
-	cout << "2. NORMAL" << endl;
-	cout << "Ai 모드를 선택하세요. " << endl;
+AI_MODE SelectAimode()
+{
+	std::cout << "======================================" << endl;
+	std::cout << " |\t 빙고 게임입니다.\t" << endl;
+	std::cout << "======================================" << endl;
+	std::cout << "빙고줄이 5줄 이상이면 게임에서 승리합니다." << endl;
+	std::cout << "======================================" << endl;
+	cout << "1. EASY " << endl; // 남은 것중에 랜덤
+	cout << "2. NORMAL " << endl; // 현재 한 줄을 완성할 수 잇는 가능성이 가장 높은 줄 선택.
+	// 3. hard: 
+	// 1) 가운데 무조건 -> 양쪽 모서리 -> 대각선 -> 가장 높은줄.
+	cout << "AI모드를 선택하세요: " << endl;
 	int iAimode;
 	cin >> iAimode;
-	// 예외 처리
-	if (iAimode < static_cast<int>(AI_MODE::AM_EASY) || 
-		iAimode > static_cast<int>(AI_MODE::AM_NORMAL)) {
+	// C, C++, C/C++
+	// 예외처리. static_cast: c++, (): c
+	if (iAimode < static_cast<int>(AI_MODE::AM_EASY) ||
+		iAimode >(int)AI_MODE::AM_NORMAL)
+	{
 		cout << "잘못 입력하셨습니다." << endl;
+
 	}
 	return (AI_MODE)iAimode;
 }
-int SelcetAiNumber(int* _pNumber, AI_MODE _eMode) {
-
-	int iNonSelect[25] = {};
-	int iNonSelectCnt = 0;
+int SelectAinumber(int* _pNumber, AI_MODE _eMode)
+{
+	int iNoneSelect[25] = {};
+	int iNoneSelectcnt = 0;
 	switch (_eMode)
 	{
-	case AI_MODE::AM_EASY: {
-		for (int i = 0; i < 25; i++) {
-			if(_pNumber[i] != INT_MAX){
-				// 숫자
-				iNonSelect[iNonSelectCnt] = _pNumber[i];
-				++iNonSelectCnt;
+	case AI_MODE::AM_EASY:
+	{
+		for (int i = 0; i < 25; i++)
+		{
+			if (_pNumber[i] != INT_MAX) // 숫자다.
+			{
+				iNoneSelect[iNoneSelectcnt] = _pNumber[i];
+				++iNoneSelectcnt;
 			}
 		}
-		return iNonSelect[rand() % iNonSelectCnt];
+		return iNoneSelect[rand() % iNoneSelectcnt];
 	}
-		break;
-	case AI_MODE::AM_NORMAL: {
+	break;
+	case AI_MODE::AM_NORMAL: // 모르겠어요 ㅠㅠㅠㅠ 리턴하는 부분에 뭘 해줘야 할지 모르겠어요
+	{
+		int aiHorStar = 0, aiVerStar = 0, aiLTStar = 0, aiRTStar = 0;
 
+		for (int i = 0; i < 5; i++)
+		{
+			aiHorStar = aiVerStar = 0;
+			for (int j = 0; j < 5; j++)
+			{
+				if (_pNumber[i * 5 + j] == INT_MAX)
+					aiHorStar++;
+				if (_pNumber[j * 5 + i] == INT_MAX)
+					aiVerStar++;
+			}
+		}
+
+		// 대각선
+		for (int i = 0; i < 25; i += 6)
+		{
+			if (_pNumber[i] == INT_MAX)
+				aiLTStar++;
+		}
+		if (aiLTStar == 5)
+		for (int i = 4; i <= 20; i += 4)
+		{
+			if (_pNumber[i] == INT_MAX)
+				aiRTStar++;
+		}
+		// 모르겠어서 하드 코딩 했어욤..ㅎㅎ
+		if (aiHorStar > aiVerStar && aiHorStar > aiLTStar && aiHorStar > aiRTStar) {
+			return;
+		}
+		if (aiVerStar > aiHorStar && aiVerStar > aiLTStar && aiVerStar > aiRTStar) {
+			return;
+		}
+		if (aiLTStar > aiHorStar && aiLTStar > aiVerStar && aiLTStar > aiRTStar) {
+			return;
+		}
+		if (aiRTStar > aiHorStar && aiRTStar > aiLTStar && aiRTStar > aiVerStar) {
+			return;
+		}
 	}
-		break;
+	break;
 	default:
 		break;
 	}
 }
-int main() {
+int main()
+{
 	srand((unsigned int)time(NULL));
 	int iNumber[25] = {};
 	int iAiNumber[25] = {};
@@ -132,54 +189,61 @@ int main() {
 	int iInput;
 	Init(iNumber);
 	Init(iAiNumber);
+	AI_MODE eAimode = SelectAimode();
 	while (true)
 	{
 		system("cls");
-		cout << "==========================player========================" << endl;
+		cout << "====================== Player =========================" << endl;
 		RenderNumber(iNumber, iBingo);
-		cout << "============================AI==========================" << endl;
-		AI_MODE eAimode = SelcetAiMode();
+
+		cout << "====================== AI ======================" << endl;
 		switch (eAimode)
 		{
 		case AI_MODE::AM_EASY:
-			cout << "AI MODE : EASY" << endl;
+			cout << "AIMode: Easy" << endl;
 			break;
 		case AI_MODE::AM_NORMAL:
-			cout << "AI MODE : EASY" << endl;
+			cout << "AIMode: Normal" << endl;
 			break;
 		default:
 			break;
 		}
 		RenderNumber(iAiNumber, iAiBingo);
-		//승리 조건 체크
-		if (iBingo >= 5) {
-			cout << "게임에서 승리하셨습니다" << endl;
+
+		// 승리 조건 체크
+		if (iBingo >= 5)
+		{
+			cout << "Player님이 게임에서 승리하셨습니다." << endl;
 			break;
 		}
-		else if (iAiBingo >= 5) {
-			cout << "AI가 게임에서 승리하였습니다.";
+		else if (iAiBingo >= 5)
+		{
+			cout << "AI가 게임에서 승리하셨습니다." << endl;
 		}
-		//숫자 게임
-		cout << "숫자를 입력하세요 ( 종료 : 0 ) : " << endl;
-		cin >> iInput;
 
-		if (iInput == 0) {
-			cout << "게임을 종료합니다. " << endl;
+		// 숫자 입력
+		cout << "숫자를 입력하세요(0: 종료): " << endl;
+		cin >> iInput;
+		if (iInput == 0)
+		{
+			cout << "게임을 종료합니다." << endl;
+			break;
 		}
-		else if (iInput < 1 || iInput > 25) {
-			cout << "잘못 입력하셨습니다" << endl;
+		else if (iInput < 1 || iInput > 25)
+		{
+			cout << "잘못 입력했습니다." << endl;
 			continue;
 		}
-		ChangeNumber(iNumber, iBingo);
-		ChangeNumber(iAiNumber, iAiBingo);
-		//ai 모드에 따라 ai가 선택할 숫자를 만들어야 함
-		iInput = SelcetAiNumber(iAiNumber, eAimode);
-		cout << "AI가 선택한 숫자는 " << iInput << "입니다" << endl;
+		ChangeNumber(iNumber, iInput);
+		ChangeNumber(iAiNumber, iInput);
+		iInput = SelectAinumber(iAiNumber, eAimode);
+		cout << "AI가 선택한 숫자는 " << iInput << "입니다." << endl;
 		Sleep(1000);
-		ChangeNumber(iNumber, iBingo);
-		ChangeNumber(iAiNumber, iAiBingo);
+		ChangeNumber(iNumber, iInput);
+		ChangeNumber(iAiNumber, iInput);
+
 		iBingo = BingoCount(iNumber);
 		iAiBingo = BingoCount(iAiNumber);
-	}
 
+	}
 }
